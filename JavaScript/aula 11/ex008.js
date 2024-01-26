a2654ca7d25596855c3854f6ef0c97cda0ad29a3
Erick Bueno <149106267+oBlindz@users.noreply.github.com>
2024-01-26 12:37:20 -0300

    Aulas de js

---
+++ JavaScript/aula 11/ex008.js
@@ -0,0 +1,9 @@
+console.log('Você entrou!')
+
+var vel = 12.5
+
+console.log(`A velocidade do seu carro foi ${vel}km/h!`)
+
+if (vel > 60) {
+    console.log(`Sua velocidade era ${vel}km/h! Você foi MULTADO!`)
+}
---
+++ JavaScript/aula 11/ex009.js
@@ -0,0 +1,9 @@
+var pais = 'Cuba'
+
+console.log(`Você nasceu em ${pais}, logo você é !`)
+
+if (pais == 'Brasil') {
+    console.log('Brasileiro')
+} else {
+    console.log('Extrangeiro')
+}
