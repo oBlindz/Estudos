2431e26553aaa2145751cec5d80e48143b2f8c96
Erick Bueno <kakeguruiso2@gmail.com>
2024-02-21 10:29:48 -0300

    outro desafio concluido!
    Tive alguns pequenos desafios, não consegui deixar tudo pronto direto, o senhor GPT me ajudou a corrigir alguns erros, no geral errei mesmo só na sintaxe do código

--- JavaScript/aula13/desafio 1/desafio.js
+++ JavaScript/aula13/desafio 1/desafio.js
@@ -1,19 +1,65 @@
 function enviar() {
     var data = new Date()
     var ano = data.getFullYear()
-    var fAno = window.document.getElementById("iidade")
+    var fAno = document.getElementById("iidade")
+    var itexto = document.getElementById("itexto")
+    var img = document.getElementById('ifoto')
 
-    if (fAno.value.length == 0 || fAno.value > ano){
+    if (fAno.value.length == 0 || fAno.value > ano) {
         window.alert('[ERRO] Verifique os dados e tente novamente')
     } else {
-        var sex = document.getElementsByClassName(`ssex`)
-        var idade = ano - Number(fAno.value)
+        var sex = document.getElementsByName('sexo')
+        var idade = ano - parseInt(fAno.value)
         var genero = ''
+
         if (sex[0].checked) {
-            var genero = 'Homem'
+            genero = 'Homem';
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.style.display = 'block'
+                img.style.backgroundSize = "cover"
+                img.style.backgroundImage = "url('criança-moço.jpg')"
+            } else if (idade < 21) {
+                //jovem
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(jovem-moço.jpg)"
+            } else if (idade < 60) {
+                //adulto
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(homem-moço.jpg)"
+            } else if (idade > 60) {
+                //idoso
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(velho-moço.jpg)"
+            }
         } else if (sex[1].checked) {
-            var genero = 'Mulher'
+            genero = 'Mulher'
+            if (idade >= 0 && idade < 10) {
+                //criança
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(criança-moça.jpg)"
+            } else if (idade < 21) {
+                //jovem
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(jovem-moça.jpg)"
+            } else if (idade < 60) {
+                //adulto
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(mulher-moça.jpg)"
+            } else if (idade > 60) {
+                //idoso
+                img.style.display = 'block'
+                img.style.backgroundSize = 'cover'
+                img.style.backgroundImage = "url(velha-moça.jpg)"
+            }
         }
-        window.alert(`Vc é ${genero} de ${idade}`)
+
+        itexto.innerHTML = `Detectamos ${genero} de ${idade} anos de idade`
     }
-}   
+}
