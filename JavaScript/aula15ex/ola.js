894f141f8efb3a2b7e4c373eb89a2764d46c494f
Erick Bueno <149106267+oBlindz@users.noreply.github.com>
2024-03-06 09:25:50 -0300

    update JavaScript/aula15ex/ola.js

--- JavaScript/aula15ex/ola.js
+++ JavaScript/aula15ex/ola.js
@@ -2,5 +2,7 @@ function adicionou(){
     let nums = ''
     if (nums.value == '' || nums.value > 100 || nums.value < 1){
     	window.alert('valor inválido')
+    } else {
+    	let nums = [window.document.getElementById('inúmero')]
     }
 }
