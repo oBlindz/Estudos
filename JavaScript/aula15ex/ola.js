beed719cbe51946894a790a9a5f338ece35d5616
Erick Bueno <kakeguruiso2@gmail.com>
2024-03-06 10:06:46 -0300

    parte inicial feito
    tive ajuda claro

--- JavaScript/aula15ex/ola.js
+++ JavaScript/aula15ex/ola.js
@@ -1,8 +1,25 @@
-function adicionou(){
-    let nums = ''
-    if (nums.value == '' || nums.value > 100 || nums.value < 1){
-    	window.alert('valor inválido')
+let num = document.querySelector('input#fnum');
+let lista = document.querySelector('select#flista');
+let res = document.querySelector('div#res');
+let valores = [];
+
+function isNumero(n){
+    if (Number(n) >=1 && Number(n) <=100) {
+        return true;
+    }
+}
+function inLista(n, l){
+    if (l.indexOf(Number(n)) != -1) {
+        return true;
     } else {
-    	let nums = [window.document.getElementById('inúmero')]
+        return false;
+    }
+}
+
+function adicionar(){
+    if (isNumero(num.value) && !inLista(num.value, valores)){
+        
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista!');
     }
 }
