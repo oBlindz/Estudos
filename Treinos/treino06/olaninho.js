086d03db1cfa733968f537ea90c7d8a73bf0e1f7
Erick Bueno <149106267+oBlindz@users.noreply.github.com>
2024-03-06 20:52:47 -0300

    update Treinos/treino06/olaninho.js

--- Treinos/treino06/olaninho.js
+++ Treinos/treino06/olaninho.js
@@ -23,16 +23,6 @@ function calc(){
         let res = n1 / (n2 * n2);
         if (res <= 15){
         window.alert(`${res.toFixed(2)}`);
-        let cor1 = document.getElementById('p1');
-        let cor2 = document.getElementById('p1-1');
-        
-        cor1.style.backgroundColor = 'rgb(212, 212, 212)';
-        cor1.style.border = 'none';
-        cor1.style.boxShadow = '3px 3px 0px rgba(212, 212, 212, 0.521)';
-        
-        cor2.style.backgroundColor = 'rgb(119, 136, 153)';
-        cor2.style.border = 'none';
-        cor2.style.boxShadow = '3px 3px 0px rgba(128, 128, 128, 0.521)';
         } else if (res <= 16){
         	window.alert(`${res.toFixed(2)}`);
         } else if (res <= 18,5){
