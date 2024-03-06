33e20ad03cfb43080b22d8d4de0c538eef52770b
Erick Bueno <149106267+oBlindz@users.noreply.github.com>
2024-03-06 09:25:09 -0300

    update JavaScript/aula15ex/ola.js

--- JavaScript/aula15ex/ola.js
+++ JavaScript/aula15ex/ola.js
@@ -1,6 +1,5 @@
 function adicionou(){
-    let nums = [window.document.getElementById('inúmero')]
-    nums.value = ''
+    let nums = ''
     if (nums.value == '' || nums.value > 100 || nums.value < 1){
     	window.alert('valor inválido')
     }
