f2f13a974566c0ef2aa069aa5ebba1cb379a63ec
Erick Bueno <kakeguruiso2@gmail.com>
2024-03-13 09:35:31 -0300

    isso ae

--- JavaScript/aula15ex/ola.js
+++ JavaScript/aula15ex/ola.js
@@ -6,6 +6,8 @@ let valores = [];
 function isNumero(n){
     if (Number(n) >=1 && Number(n) <=100) {
         return true;
+    } else {
+        return false;
     }
 }
 function inLista(n, l){
@@ -18,8 +20,37 @@ function inLista(n, l){
 
 function adicionar(){
     if (isNumero(num.value) && !inLista(num.value, valores)){
-        
+        valores.push(Number(num.value));
+        let item = document.createElement('option');
+        item.text = `Valor ${num.value} adicionado`;
+        lista.appendChild(item);
+        res.innerHTML = '';
     } else {
         window.alert('Valor inválido ou já encontrado na lista!');
+    }
+    num.value = '';
+    num.focus();
+}
+
+function finalizar(){
+    if (valores.length == 0){
+        window.alert('Adicione valores ai po!');
+    } else {
+        let tot = valores.length;
+        let maior = valores[0];
+        let menor = valores[0];
+        
+        for(let pos in valores){
+            if (valores[pos] > maior){
+                maior = valores[pos];
+            } else if (valores[pos] < menor){
+                menor = valores[pos];
+            }
+        }
+
+        res.innerHTML = '';
+        res.innerHTML += `<p>Ao todo temos ${tot} números registrados!</p>`;
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`;
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`;
     }
 }
