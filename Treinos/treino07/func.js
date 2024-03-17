6a5b7e604fb68dca75c0379d8d93877a6456c552
Erick Bueno <kakeguruiso2@gmail.com>
2024-03-17 08:10:42 -0300

    desafio refeito do 0
    Fiz sem ajuda do chat gpt, acabou essa mamada

--- Treinos/treino07/func.js
+++ Treinos/treino07/func.js
@@ -1,11 +1,13 @@
-// Fazer o Check ganhar sempre 1 e se for impar é branco e se for par é preto
-function clc(){
-    var checkBox = document.getElementById('check');
-    var fundo = document.querySelector('div#container');
+function check(){
+    var checkBox = document.querySelector('input#check');
+    var container = document.querySelector('div.container');
 
     if(checkBox.checked){
-        document.body.style.backgroundColor='rgb(56, 56, 56)';
+        document.body.style.backgroundColor='rgb(20, 20, 20)';
+        document.body.style.transition='0.5s'
+        container.style.cssText='background-color:rgb(34,34,34); border:5px solid rgba(10, 10, 10, 0.5); box-shadow:0px 0px 10px rgba(50, 50, 253, 0.562); transition:0.5s;';
     } else {
-        document.body.style.backgroundColor='rgb(230, 226, 230)';
+        document.body.style.backgroundColor='white';
+        container.style.cssText='background-color:rgb(214, 214, 214); '
     }
 }
