4497fb5f59aef989635dd89e3053223551ad085d
Erick Bueno <kakeguruiso2@gmail.com>
2024-03-24 11:48:51 -0300

    barra de pesquisa estética feita

---
+++ Treinos/treino10/index.js
@@ -0,0 +1,11 @@
+function pesquisar(){
+    window.alert('a funcionalidade desse exercício é puramente estética');
+}
+function entrou(){
+    let container = document.querySelector('div.container');
+    container.style.cssText='box-shadow:0px 0px 10px orange;'
+}
+function saiu(){
+    let container = document.querySelector('div.container');
+    container.style.cssText='box-shadow:none;'
+}
